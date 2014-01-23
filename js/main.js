16a41390589d4f1ee9c64f54c10598c4e9168d2f
Ryan Brooks <ryanbrooksis@gmail.com>
2014-01-23 22:14:36 +0000

    a couple of hours of hacking. Working but not much error handling, and needs a little TLC on the styling

--- js/main.js
+++ js/main.js
@@ -1,1 +1,37 @@
+var app = angular.module('calculator', []);
+
+app.filter('zeroNull', function() {
+  return function(input) {
+  	if(input == undefined || input == null || isNaN(input)){
+  		return "0";
+  	}else{
+  		return input;
+  	}
+  };
+});
+
+app.controller('CalculatorCtrl', function($scope) {
+	var importedScope = angular.fromJson(sessionStorage.calculator);
+	//$scope = importedScope ? importedScope : $scope 
+	$scope.dayRate = importedScope.dayRate;
+    $scope.totalCost = "---";
+	$scope.twoRate = false;
+
+	$scope.calculate = function(){
+		$scope.twoRate = !($scope.nightRate == "" || $scope.nightRate == undefined || $scope.nightRate == null || isNaN($scope.nightRate));
+		$scope.dayKwh = $scope.dayReading - $scope.prevDayReading;
+		$scope.dayCost = $scope.dayKwh * $scope.dayRate;
+
+		$scope.nightKwh = $scope.nightReading - $scope.prevNightReading;
+		$scope.nightCost = $scope.nightKwh * $scope.nightRate;
+
+		if($scope.twoRate){
+			$scope.totalCost = $scope.dayCost + $scope.nightCost;
+		}else{
+			$scope.totalCost = $scope.dayCost
+		}
+
+		sessionStorage.calculator = angular.toJson($scope);
+	}
+});
 
